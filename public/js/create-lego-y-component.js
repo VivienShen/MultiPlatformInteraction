d16aac36fdd03135ef7f8d4dbbc729e4d847fd8e
VivienShen <vivienshen88@gmail.com>
2020-01-25 02:10:25 -0500

    minor adjustments

--- public/js/create-lego-y-component.js
+++ public/js/create-lego-y-component.js
@@ -8,7 +8,7 @@ AFRAME.registerComponent('create-lego-y-component', {
         //add event listener for "click" event on whatever entity has this component
         Context_AF.el.addEventListener('click', function(event){
             console.log("clicked!!!!");
-            Context_AF.createLego();
+            Context_AF.createLego1();
         });
 
         //when "hovering" make larger
@@ -35,7 +35,7 @@ AFRAME.registerComponent('create-lego-y-component', {
         lego1Elem.setAttribute('material', 'color', 'yellow');  //set material/texture
 
         //random tranform
-        lego1Elem.setAttribute('position', {x:(Math.random() * 6.0) - 3.0, y:0, z:-4.0 - (Math.random() * 3.0)}); //random x around axis and random z behind button
+        lego1Elem.setAttribute('position', {x:(Math.random() * 6.0) - 3.0, y:0, z:-3.0 - (Math.random() * 3.0)}); //random x around axis and random z behind button
         const randScale = 0.2 + (Math.random() * 0.8);
         lego1Elem.setAttribute('scale', {x:0.5, y:0.5, z:0.5});  //scale
         lego1Elem.setAttribute('rotation', {x:0, y:0, z:0});   //random y rotation
---
+++ public/js/delete-lego-y-component.js
@@ -0,0 +1,32 @@
+'use strict'
+
+AFRAME.registerComponent('delete-lego-y-component', {
+    schema: {},
+    init : function() {
+        const Context_AF = this; //refers to "this" component
+
+        //add event listener for "click" event on whatever entity has this component
+        Context_AF.el.addEventListener('click', function(event){
+            console.log("clicked!!!!");
+            Context_AF.deleteMyself();
+            
+        });
+
+        //when "hovering" make larger
+        Context_AF.el.addEventListener('mouseenter', function(event){
+            //el = element or entity
+            //object3D = three.js 3D geometry object
+            //scale = three.js vector that represents scale
+            Context_AF.el.object3D.scale.set(1.1, 1.1, 1.1);
+        });
+        //original unhovered size
+        Context_AF.el.addEventListener('mouseleave', function(event){
+            Context_AF.el.object3D.scale.set(1.0, 1.0, 1.0);
+        });
+    },
+    deleteMyself : function(){
+        const Context_AF = this;
+        Context_AF.el.parentNode.removeChild(Context_AF.el); //only a parent can remove their childNodes
+    }
+    
+})
